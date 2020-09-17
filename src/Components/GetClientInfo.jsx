11fbfce786742c6c10e48d442aa48c5251514a1d
jbixon13 <jbixon13@gmail.com>
2020-09-16 21:58:05 -0400

    add weather api function

--- src/Components/GetClientInfo.jsx
+++ src/Components/GetClientInfo.jsx
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from 'react'
 
 var SunCalc = require('suncalc');
 
+const WEATHER_API = process.env.REACT_APP_WEATHER_API
+
 function GetClientInfo() {
     const [time, setTime] = useState(new Date());
     const [{lat, lng}, setLocation] = useState({lat: 0, lng: 0});
+    const [weather, setWeather] = useState([]);
 
+    // get local time of client on set interval (every minute)
     useEffect(() => {
         const interval = setInterval(() => {
             setTime(new Date());
-            console.log(time)
         }, 60000);
         return () => clearInterval(interval);
     }, [time]);
 
+    // get location of client to determine local sun calculations & weather
     useEffect(() => {
         if(navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
@@ -28,6 +32,22 @@ function GetClientInfo() {
         }
     }, [lat, lng]);
 
+    // get current weather of client through API call to weatherapi.com
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${WEATHER_API}&q=${lat},${lng}`)
+                const json = await response.json();
+                console.log(json.current.condition.text);
+                setWeather(json.current.condition.text);
+            } catch (error) {
+                console.log('Weather API call failed');
+            }
+        }
+        fetchData();
+    }, [lat, lng])
+
+    // get sun calculations based on prior functions
     const sunTimes = SunCalc.getTimes(time, lat, lng);
 
     return(
@@ -36,6 +56,7 @@ function GetClientInfo() {
             <p>The user's location is: {[lat, lng]}</p>
             <p>The sunrise time at the user's location is: {sunTimes.sunrise.toLocaleTimeString()}</p>
             <p>The sunset time at the user's location is: {sunTimes.sunset.toLocaleTimeString()}</p>
+            <p>The weather at the user's location is: {weather}</p>
         </div>
     )
 }
