6ad1e074bd552006478580256fa0578116a078d6
jbixon13 <jbixon13@gmail.com>
2020-09-16 23:59:16 -0400

    change lat/lng display

--- src/Components/GetClientInfo.jsx
+++ src/Components/GetClientInfo.jsx
@@ -52,7 +52,7 @@ function GetClientInfo() {
     return(
         <div>
             <p>The user's current time is: {time.toLocaleTimeString()}</p>
-            <p>The user's location is: {[lat, lng]}</p>
+            <p>The user's location is: {lat}, {lng}</p>
             <p>The sunrise time at the user's location is: {sunTimes.sunrise.toLocaleTimeString()}</p>
             <p>The sunset time at the user's location is: {sunTimes.sunset.toLocaleTimeString()}</p>
             <p>The weather at the user's location is: {weather}</p>
