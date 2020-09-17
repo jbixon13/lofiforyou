1a706764d7addbb998ea441bf14d19232211f316
jbixon13 <jbixon13@gmail.com>
2020-09-16 23:38:09 -0400

    add to r3f canvas demo

--- src/Components/LofiCanvas.jsx
+++ src/Components/LofiCanvas.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import { Canvas } from 'react-three-fiber'
+import { OrbitControls, Sky } from 'drei'
+
+const Plane = () => (
+    <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -2.5, 0]}>
+        <planeBufferGeometry attach='geometry' args={[100, 100]} />
+        <meshPhysicalMaterial attach='material' color='pink' />
+    </mesh>
+)
+
+const Box = () => (
+    <mesh castShadow>
+        <boxBufferGeometry attach='geometry' args={[5, 5, 5]} />
+        <meshBasicMaterial attach='material' color='black' />
+    </mesh>
+)
 
 function LofiCanvas() {
     return(
-        <Canvas>
-            <mesh>
-                <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-                <meshBasicMaterial attach='material' color='red' />
-            </mesh>
+        <Canvas shadowMap camera={{ position: [0, 10, 15] }}>
+            <OrbitControls />
+            <Box />
+            <Plane />
+            <ambientLight />
+            <spotLight
+                castShadow
+                penumbra={1}
+                position={[0, 5, 10]}
+            />
+            <Sky />
         </Canvas>
     )
 }
