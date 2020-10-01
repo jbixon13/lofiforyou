141f981acebead95b75fc04ff88c95f4ca92b082
jbixon13 <jbixon13@gmail.com>
2020-09-30 23:11:06 -0400

    so close -- rain falls but then keeps falling

--- src/Components/LofiCanvas.jsx
+++ src/Components/LofiCanvas.jsx
@@ -1,8 +1,7 @@
-import React, { useRef } from 'react'
+import React, { useRef, useMemo } from 'react'
 import { Canvas, useFrame, extend } from 'react-three-fiber'
 import { OrbitControls, Sky, Stars, MeshDistortMaterial, shaderMaterial } from 'drei'
 import * as THREE from 'three'
-
 
 const Plane = () => (
     <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
@@ -91,6 +90,70 @@ const ShaderBox = () => {
     )
 }
 
+const Rain = ({ rainCount }) => {
+    const initialPositions = []
+    const initialVelocities = []
+    const initialAccelerations = []
+    for (let i=0; i < rainCount; i++) {
+        initialPositions.push(-5 + Math.random() * 10)
+        initialPositions.push(10 + Math.random() * 2)
+        initialPositions.push(Math.random() * 5)
+        initialVelocities.push(0)
+        initialVelocities.push(-1)
+        initialVelocities.push(0)
+        initialAccelerations.push(0)
+        initialAccelerations.push(3.8)
+        initialAccelerations.push(0)
+    }
+
+    const positions = useMemo(() => new Float32Array(initialPositions), [initialPositions])
+    const velocities = useMemo(() => new Float32Array(initialVelocities), [initialVelocities])
+    const accelerations = useMemo(() => new Float32Array(initialAccelerations), [initialAccelerations])
+    const uniforms = useMemo(() => ({ time: { value: 1.0 } }), [])
+
+    const geom = useRef()
+
+    const vert = `uniform float time;
+    attribute vec3 velocity;
+    attribute vec3 acceleration;
+    void main() {
+        vec3 pos = position;
+        gl_Position = projectionMatrix 
+            * modelViewMatrix
+            * vec4(
+                vec3(
+                    pos[0],
+                    pos[1] + (time * velocity[1] * acceleration[1]),
+                    pos[2]), 1.0);
+        gl_PointSize = 5.0;
+    }`
+
+    const frag = `uniform float time;
+    void main() {
+        float z = 1.0 - (gl_FragCoord.z / gl_FragCoord.w) / 20.0;
+        gl_FragColor = vec4(sin(time * 2.0) * z, cos(time) * z, atan(time) * z, 1.0);
+    }`
+
+    useFrame(({ clock }) => {
+        if (geom.current) {
+            geom.current.material.uniforms.time.value = clock.getElapsedTime()
+            geom.current.geometry.verticesNeedUpdate = true
+            // geom.current.geometry.attributes.position.setY(1, 20)
+        }
+    });
+
+    return(
+        <points ref={geom}>
+            <bufferGeometry attach="geometry">
+                <bufferAttribute attachObject={["attributes", "position"]} count={positions.length / 3} array={positions} itemSize={3} />
+                <bufferAttribute attachObject={["attributes", "velocity"]} count={velocities.length / 3} array={velocities} itemSize={3} />
+                <bufferAttribute attachObject={["attributes", "acceleration"]} count={accelerations.length / 3} array={accelerations} itemSize={3} />
+            </bufferGeometry>
+            <shaderMaterial attach="material" uniforms={uniforms} vertexShader={vert} fragmentShader={frag} vertexColors />
+        </points>
+    )
+}
+
 const Skybox = ({isDay}) => {
 
     var effectController = {
@@ -127,7 +190,7 @@ function LofiCanvas({isDay}) {
             <OrbitControls autoRotate={false} />
             <Clouds />
             <Plane />
-            <ShaderBox />
+            <Rain rainCount={50} />
             <ambientLight args={['white', 0.5]} />
             <pointLight
                 castShadow
