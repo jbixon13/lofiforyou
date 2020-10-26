c983e85ef0fbfd056171d51bdd5177fbc5276177
jbixon13 <jbixon13@gmail.com>
2020-10-25 21:12:39 -0400

    clean up client info

--- src/Components/ClientInfo.jsx
+++ src/Components/ClientInfo.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 
 function ClientInfo({time, isDay, sunPhase, sunPosition, lat, lng, sunTimes, weather}) {
     return(
-        <div>
-            <p>The user's current time is: {time.toLocaleTimeString()}</p>
-            <p>The user's time of day is: {isDay ? 'daytime' : 'nighttime'}</p>
-            <p>The user's phase of the day is: {sunPhase}</p>
-            <p>The user's current sun azimuth is: {sunPosition.azimuth}</p>
-            <p>The user's current sun altitude is: {sunPosition.altitude}</p>
-            <p>The user's location is: {lat}, {lng}</p>
-            <p>The sunrise time at the user's location is: {sunTimes.sunrise.toLocaleTimeString()}</p>
-            <p>The sunset time at the user's location is: {sunTimes.sunset.toLocaleTimeString()}</p>
-            <p>The weather at the user's location is: {weather}</p>
+        <div className='client-info'>
+            <h4>Debugging:</h4>
+            <p><strong>Local time:</strong> {time.toLocaleTimeString()}</p>
+            <p><strong>Location:</strong> {lat}, {lng}</p>
+            <p><strong>Day or night:</strong> {isDay ? 'daytime' : 'nighttime'}</p>
+            <p><strong>Time of day:</strong> {sunPhase}</p>
+            <p><strong>Phases of day:</strong></p>
+            <ul>
+                <li><strong>Sunrise:</strong> {sunTimes.sunrise.toLocaleTimeString()} - {sunTimes.sunriseEnd.toLocaleTimeString()}</li>
+                <li><strong>Daytime:</strong> {sunTimes.sunriseEnd.toLocaleTimeString()} - {sunTimes.sunsetStart.toLocaleTimeString()}</li>
+                <li><strong>Sunset:</strong> {sunTimes.sunsetStart.toLocaleTimeString()} - {sunTimes.sunset.toLocaleTimeString()}</li>
+                <li><strong>Dusk:</strong> {sunTimes.sunset.toLocaleTimeString()} - {sunTimes.dusk.toLocaleTimeString()}</li>
+                <li><strong>Night: </strong> {sunTimes.dusk.toLocaleTimeString()} - {sunTimes.sunrise.toLocaleTimeString()}</li>
+            </ul>
+            <p><strong>Weather:</strong> {weather}</p>
         </div>
     )
 }
