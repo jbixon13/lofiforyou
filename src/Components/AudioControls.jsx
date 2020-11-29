0718584abb45488c47bacc9ee37e4d1b51e1ebb0
jbixon13 <jbixon13@gmail.com>
2020-11-28 21:31:31 -0500

    add looping functionality & style

--- src/Components/AudioControls.jsx
+++ src/Components/AudioControls.jsx
@@ -4,6 +4,7 @@ import ReactPlayer from 'react-player/youtube'
 function AudioControls({getLocation}) {
     const [url, setUrl] = useState('https://www.youtube.com/watch?v=5qap5aO4i9A');
     const [playing, setPlaying] = useState(false);
+    const [looping, setLooping] = useState(false);
 
     const playHandler = () => {
         setPlaying(!playing);
@@ -13,16 +14,22 @@ function AudioControls({getLocation}) {
         setUrl(event.target.value)
     }
 
+    const loopHandler = () => {
+        setLooping(!looping)
+    }
+
     return(
         <div className='audio-controls'>
-            <ReactPlayer className='audio-player'
+            <ReactPlayer
                 url={url} 
                 playing={playing}
+                loop={looping}
                 width={0}
                 height={0}
             />
             <button onClick={getLocation}>Create Scene</button>
             <button onClick={playHandler}>{playing ? 'Pause' : 'Play'}</button>
+            <button onClick={loopHandler}>{looping ? 'Looping' : 'Not Looping'}</button>
             <input onChange={urlHandler} type='text' placeholder='Paste any Youtube video or playlist url here' />
         </div>
     )
