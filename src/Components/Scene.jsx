9f178df197b2d3889531d29b22f637ef7d4bdc7d
jbixon13 <jbixon13@gmail.com>
2020-12-30 15:55:57 -0500

    add parallax effect to scene

---
+++ src/Components/Scene.jsx
@@ -0,0 +1,50 @@
+import React, { useRef, Suspense } from "react";
+import { useFrame, useThree } from "react-three-fiber";
+import { OrbitControls } from "@react-three/drei";
+import Skybox from "./Skybox";
+import Weather from "./Weather";
+import Lowpoly from './Lowpoly'
+import * as THREE from "three";
+
+const Scene = ({ isDay, sunPhase, weather }) => {
+    const group = useRef();
+    const rotationEuler = new THREE.Euler(0, 0, 0);
+    const rotationQuaternion = new THREE.Quaternion(0, 0, 0, 0);
+    const { viewport } = useThree();
+
+    useFrame(({ mouse }) => {
+        const x = (mouse.x * viewport.width) / 200;
+        const y = (mouse.y * viewport.height) / 200;
+
+        rotationEuler.set(y, x, 0);
+        rotationQuaternion.setFromEuler(rotationEuler);
+
+        group.current.quaternion.slerp(rotationQuaternion, 0.1);
+    });
+
+    return(
+        <>
+            <group ref={group}>
+                <OrbitControls 
+                    autoRotate={false}
+                    target={[0, 3, 0]}
+                    minPolarAngle={Math.PI / 3}
+                    maxPolarAngle={Math.PI / 2}
+                    minAzimuthAngle={-Math.PI / 4}
+                    maxAzimuthAngle={Math.PI / 4}
+                    enableZoom={false}
+                    enablePan={false}
+                    enableRotate={false}
+                />
+                <Weather weather={weather} />
+                <Suspense fallback={null} >
+                    <Lowpoly position={[1, 0, 8]}/>
+                </Suspense>
+                <Skybox isDay={isDay} sunPhase={sunPhase} weather={weather} />
+                {/* <Fog isDay={isDay} /> */}
+                {/* <gridHelper args={[30, 30, 30]} /> */}
+            </group>
+        </>
+    )
+}
+export default Scene
--- src/Components/ThreeCanvas.jsx
+++ src/Components/ThreeCanvas.jsx
@@ -1,31 +1,11 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { Canvas } from 'react-three-fiber'
-import { OrbitControls } from '@react-three/drei'
-import Skybox from './Skybox'
-import Weather from './Weather'
-// import Fog from './Fog'
-import Lowpoly from './Lowpoly'
+import Scene from './Scene'
 
 function ThreeCanvas({ isDay, sunPhase, weather }) {
     return(
         <Canvas className={isDay ? 'day' : 'night'} colorManagement shadowMap camera={{ position: [0, 2, 12] }}>
-            <OrbitControls 
-                autoRotate={false}
-                target={[0, 3, 0]}
-                minPolarAngle={Math.PI / 3}
-                maxPolarAngle={Math.PI / 2}
-                minAzimuthAngle={-Math.PI / 4}
-                maxAzimuthAngle={Math.PI / 4}
-                enableZoom={false}
-                enablePan={false}
-            />
-            <Weather weather={weather} />
-            {/* <Fog isDay={isDay} /> */}
-            <Suspense fallback={null} >
-                <Lowpoly position={[1, 0, 8]}/>
-            </Suspense>
-            <Skybox isDay={isDay} sunPhase={sunPhase} weather={weather} />
-            {/* <gridHelper args={[30, 30, 30]} /> */}
+            <Scene isDay={isDay} sunPhase={sunPhase} weather={weather} />
         </Canvas>
     )
 }
